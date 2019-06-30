371dfaffa2f0f4b138e6e2bc48394a16e7eb830b
Yuta Iwama <ganmacs@gmail.com>
2019-06-30 23:16:46 +0900

    Change directory layout

--- tests/unpack.rs
+++ tests/unpack.rs
@@ -219,3 +219,27 @@ fn unpack_value() {
         Value::Array(vec![Value::from(1 as u8), Value::from(2 as u8)])
     );
 }
+
+#[test]
+fn unpack_invalid_str_value() {
+    let v: Vec<u8> = vec![
+        219, 0, 0, 0, 52, 146, 215, 0, 93, 24, 122, 26, 4, 114, 82, 40, 129, 167, 109, 101, 115,
+        115, 97, 103, 101, 165, 100, 117, 109, 109, 121, 146, 215, 0, 93, 24, 122, 27, 5, 181, 62,
+        32, 129, 167, 109, 101, 115, 115, 97, 103, 101, 165, 100, 117, 109, 109, 121,
+    ];
+
+    let mut reader = io::Cursor::new(&v);
+
+    let mut writer = vec![];
+    let val = msgpack::unpack_value(&mut reader).unwrap();
+    if let Value::String(ref s) = &val {
+        assert!(s.is_err());
+        msgpack::pack_value(&mut writer, val).unwrap();
+
+        // right skip 1 bytes which is header (fixstring)
+        // left: skip 4 bytes (219, 0, 0, 0, 52) which is header (str32)
+        assert_eq!(writer[1..(writer.len())], v[4..(v.len())]);
+    } else {
+        panic!("must be string");
+    };
+}
