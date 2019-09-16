b6429e93839ad2dd836cc493ef65ad1af890c03a
Yuta Iwama <ganmacs@gmail.com>
2019-09-16 16:49:15 +0900

    define MessagePacker trait

--- src/lib.rs
+++ src/lib.rs
@@ -49,3 +49,36 @@ impl<'a> BufferedRead<'a> for Cursor<&'a [u8]> {
         self.set_position(pos + len as u64);
     }
 }
+
+pub trait MessagePacker {
+    fn pack_nil(&mut self) -> Result<(), PackError>;
+    fn pack_uint(&mut self, v: u64) -> Result<(), PackError>;
+    fn pack_u8(&mut self, v: u8) -> Result<(), PackError>;
+    fn pack_from_u8(&mut self, v: u8) -> Result<(), PackError>;
+    fn pack_from_u16(&mut self, v: u16) -> Result<(), PackError>;
+    fn pack_u16(&mut self, v: u16) -> Result<(), PackError>;
+    fn pack_from_u32(&mut self, v: u32) -> Result<(), PackError>;
+    fn pack_u32(&mut self, v: u32) -> Result<(), PackError>;
+    fn pack_from_u64(&mut self, v: u64) -> Result<(), PackError>;
+    fn pack_u64(&mut self, v: u64) -> Result<(), PackError>;
+    fn pack_int(&mut self, v: i64) -> Result<(), PackError>;
+    fn pack_from_i8(&mut self, v: i8) -> Result<(), PackError>;
+    fn pack_i8(&mut self, v: i8) -> Result<(), PackError>;
+    fn pack_from_i16(&mut self, v: i16) -> Result<(), PackError>;
+    fn pack_i16(&mut self, v: i16) -> Result<(), PackError>;
+    fn pack_from_i32(&mut self, v: i32) -> Result<(), PackError>;
+    fn pack_i32(&mut self, v: i32) -> Result<(), PackError>;
+    fn pack_from_i64(&mut self, v: i64) -> Result<(), PackError>;
+    fn pack_i64(&mut self, v: i64) -> Result<(), PackError>;
+    fn pack_f32(&mut self, v: f32) -> Result<(), PackError>;
+    fn pack_f64(&mut self, v: f64) -> Result<(), PackError>;
+    fn pack_bool(&mut self, v: bool) -> Result<(), PackError>;
+    fn pack_str(&mut self, v: &str) -> Result<(), PackError>;
+    fn pack_bin(&mut self, v: &[u8]) -> Result<(), PackError>;
+    fn pack_array_header(&mut self, len: usize) -> Result<(), PackError>;
+    fn pack_map_header(&mut self, len: usize) -> Result<(), PackError>;
+    fn pack_ext_header(&mut self, ext_type: i8, len: usize) -> Result<(), PackError>;
+    // this method is used after pack_ext_header
+    fn write_payload(&mut self, v: &[u8]) -> Result<(), PackError>;
+    fn flush(&mut self) -> Result<(), PackError>;
+}
--- src/packer.rs
+++ src/packer.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use crate::{pack, PackError, Value};
+use crate::{pack, MessagePacker, PackError, Value};
 
 #[derive(Debug)]
 pub struct Packer<W> {
@@ -26,122 +26,125 @@ impl<W> Packer<W> {
 }
 
 impl<W: io::Write> Packer<W> {
-    pub fn pack_nil(&mut self) -> Result<(), PackError> {
-        pack::pack_nil(&mut self.wr)
-    }
-
-    pub fn pack_uint(&mut self, v: u64) -> Result<(), PackError> {
-        pack::pack_from_u64(&mut self.wr, v)
-    }
-
-    pub fn pack_u8(&mut self, v: u8) -> Result<(), PackError> {
-        pack::pack_u8(&mut self.wr, v)
-    }
-
-    pub fn pack_from_u8(&mut self, v: u8) -> Result<(), PackError> {
-        pack::pack_from_u8(&mut self.wr, v)
-    }
-
-    pub fn pack_from_u16(&mut self, v: u16) -> Result<(), PackError> {
-        pack::pack_from_u16(&mut self.wr, v)
-    }
-
-    pub fn pack_u16(&mut self, v: u16) -> Result<(), PackError> {
-        pack::pack_u16(&mut self.wr, v)
-    }
-
-    pub fn pack_from_u32(&mut self, v: u32) -> Result<(), PackError> {
-        pack::pack_from_u32(&mut self.wr, v)
-    }
-
-    pub fn pack_u32(&mut self, v: u32) -> Result<(), PackError> {
-        pack::pack_u32(&mut self.wr, v)
-    }
-
-    pub fn pack_from_u64(&mut self, v: u64) -> Result<(), PackError> {
-        pack::pack_from_u64(&mut self.wr, v)
-    }
-
-    pub fn pack_u64(&mut self, v: u64) -> Result<(), PackError> {
-        pack::pack_u64(&mut self.wr, v)
-    }
-
-    pub fn pack_int(&mut self, v: i64) -> Result<(), PackError> {
-        pack::pack_from_i64(&mut self.wr, v)
-    }
-
-    pub fn pack_from_i8(&mut self, v: i8) -> Result<(), PackError> {
-        pack::pack_from_i8(&mut self.wr, v)
-    }
-    pub fn pack_i8(&mut self, v: i8) -> Result<(), PackError> {
-        pack::pack_i8(&mut self.wr, v)
-    }
-
-    pub fn pack_from_i16(&mut self, v: i16) -> Result<(), PackError> {
-        pack::pack_from_i16(&mut self.wr, v)
-    }
-
-    pub fn pack_i16(&mut self, v: i16) -> Result<(), PackError> {
-        pack::pack_i16(&mut self.wr, v)
-    }
-
-    pub fn pack_from_i32(&mut self, v: i32) -> Result<(), PackError> {
-        pack::pack_from_i32(&mut self.wr, v)
-    }
-
-    pub fn pack_i32(&mut self, v: i32) -> Result<(), PackError> {
-        pack::pack_i32(&mut self.wr, v)
-    }
-
-    pub fn pack_from_i64(&mut self, v: i64) -> Result<(), PackError> {
-        pack::pack_from_i64(&mut self.wr, v)
-    }
-
-    pub fn pack_i64(&mut self, v: i64) -> Result<(), PackError> {
-        pack::pack_i64(&mut self.wr, v)
-    }
-
-    pub fn pack_f32(&mut self, v: f32) -> Result<(), PackError> {
-        pack::pack_f32(&mut self.wr, v)
-    }
-
-    pub fn pack_f64(&mut self, v: f64) -> Result<(), PackError> {
-        pack::pack_f64(&mut self.wr, v)
-    }
-
-    pub fn pack_bool(&mut self, v: bool) -> Result<(), PackError> {
-        pack::pack_bool(&mut self.wr, v)
-    }
-
-    pub fn pack_str(&mut self, v: &str) -> Result<(), PackError> {
-        pack::pack_str(&mut self.wr, v)
-    }
-
-    pub fn pack_bin(&mut self, v: &[u8]) -> Result<(), PackError> {
-        pack::pack_bin(&mut self.wr, v)
-    }
-
-    pub fn pack_array_header(&mut self, len: usize) -> Result<(), PackError> {
-        pack::pack_array_header(&mut self.wr, len)
-    }
-
-    pub fn pack_map_header(&mut self, len: usize) -> Result<(), PackError> {
-        pack::pack_map_header(&mut self.wr, len)
-    }
-
-    pub fn pack_ext_header(&mut self, ext_type: i8, len: usize) -> Result<(), PackError> {
-        pack::pack_ext_header(&mut self.wr, ext_type, len)
-    }
-
     pub fn pack_value(&mut self, v: Value) -> Result<(), PackError> {
         pack::pack_value(&mut self.wr, v)
     }
+}
 
-    pub fn write_payload(&mut self, v: &[u8]) -> Result<(), PackError> {
+impl<W: io::Write> MessagePacker for Packer<W> {
+    fn pack_nil(&mut self) -> Result<(), PackError> {
+        pack::pack_nil(&mut self.wr)
+    }
+
+    fn pack_uint(&mut self, v: u64) -> Result<(), PackError> {
+        pack::pack_from_u64(&mut self.wr, v)
+    }
+
+    fn pack_u8(&mut self, v: u8) -> Result<(), PackError> {
+        pack::pack_u8(&mut self.wr, v)
+    }
+
+    fn pack_from_u8(&mut self, v: u8) -> Result<(), PackError> {
+        pack::pack_from_u8(&mut self.wr, v)
+    }
+
+    fn pack_from_u16(&mut self, v: u16) -> Result<(), PackError> {
+        pack::pack_from_u16(&mut self.wr, v)
+    }
+
+    fn pack_u16(&mut self, v: u16) -> Result<(), PackError> {
+        pack::pack_u16(&mut self.wr, v)
+    }
+
+    fn pack_from_u32(&mut self, v: u32) -> Result<(), PackError> {
+        pack::pack_from_u32(&mut self.wr, v)
+    }
+
+    fn pack_u32(&mut self, v: u32) -> Result<(), PackError> {
+        pack::pack_u32(&mut self.wr, v)
+    }
+
+    fn pack_from_u64(&mut self, v: u64) -> Result<(), PackError> {
+        pack::pack_from_u64(&mut self.wr, v)
+    }
+
+    fn pack_u64(&mut self, v: u64) -> Result<(), PackError> {
+        pack::pack_u64(&mut self.wr, v)
+    }
+
+    fn pack_int(&mut self, v: i64) -> Result<(), PackError> {
+        pack::pack_from_i64(&mut self.wr, v)
+    }
+
+    fn pack_from_i8(&mut self, v: i8) -> Result<(), PackError> {
+        pack::pack_from_i8(&mut self.wr, v)
+    }
+    fn pack_i8(&mut self, v: i8) -> Result<(), PackError> {
+        pack::pack_i8(&mut self.wr, v)
+    }
+
+    fn pack_from_i16(&mut self, v: i16) -> Result<(), PackError> {
+        pack::pack_from_i16(&mut self.wr, v)
+    }
+
+    fn pack_i16(&mut self, v: i16) -> Result<(), PackError> {
+        pack::pack_i16(&mut self.wr, v)
+    }
+
+    fn pack_from_i32(&mut self, v: i32) -> Result<(), PackError> {
+        pack::pack_from_i32(&mut self.wr, v)
+    }
+
+    fn pack_i32(&mut self, v: i32) -> Result<(), PackError> {
+        pack::pack_i32(&mut self.wr, v)
+    }
+
+    fn pack_from_i64(&mut self, v: i64) -> Result<(), PackError> {
+        pack::pack_from_i64(&mut self.wr, v)
+    }
+
+    fn pack_i64(&mut self, v: i64) -> Result<(), PackError> {
+        pack::pack_i64(&mut self.wr, v)
+    }
+
+    fn pack_f32(&mut self, v: f32) -> Result<(), PackError> {
+        pack::pack_f32(&mut self.wr, v)
+    }
+
+    fn pack_f64(&mut self, v: f64) -> Result<(), PackError> {
+        pack::pack_f64(&mut self.wr, v)
+    }
+
+    fn pack_bool(&mut self, v: bool) -> Result<(), PackError> {
+        pack::pack_bool(&mut self.wr, v)
+    }
+
+    fn pack_str(&mut self, v: &str) -> Result<(), PackError> {
+        pack::pack_str(&mut self.wr, v)
+    }
+
+    fn pack_bin(&mut self, v: &[u8]) -> Result<(), PackError> {
+        pack::pack_bin(&mut self.wr, v)
+    }
+
+    fn pack_array_header(&mut self, len: usize) -> Result<(), PackError> {
+        pack::pack_array_header(&mut self.wr, len)
+    }
+
+    fn pack_map_header(&mut self, len: usize) -> Result<(), PackError> {
+        pack::pack_map_header(&mut self.wr, len)
+    }
+
+    fn pack_ext_header(&mut self, ext_type: i8, len: usize) -> Result<(), PackError> {
+        pack::pack_ext_header(&mut self.wr, ext_type, len)
+    }
+
+    // this method is used after pack_ext_header
+    fn write_payload(&mut self, v: &[u8]) -> Result<(), PackError> {
         pack::write_all(&mut self.wr, v)
     }
 
-    pub fn flush(&mut self) -> Result<(), PackError> {
+    fn flush(&mut self) -> Result<(), PackError> {
         self.wr.flush().map_err(PackError::WriteError)
     }
 }
